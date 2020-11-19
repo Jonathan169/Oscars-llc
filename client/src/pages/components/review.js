d608fd4f8d14d6bd6719bb0ccd50171666a71608
Jonathan Hernandez <jon.hernandez169@gmail.com>
2020-11-19 12:00:54 -0600

    first commit

---
+++ client/src/pages/components/review.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class review extends React.Component{
+    state={}
+
+    render(){
+        return (
+            <div className="">
+
+            </div>
+        )
+    }
+}
+export default review;
